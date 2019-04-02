a78b068b0f002a76ae26aec0cf0dbcf759f99b03
Paarth <paarth.arora21@gmail.com>
2019-04-01 22:00:37 -0400

    added in projection matrix

--- EDP Unity/Assets/Scripts/WebCamera.cs
+++ EDP Unity/Assets/Scripts/WebCamera.cs
@@ -5,30 +5,28 @@ using UnityEngine.UI;
 
 public class WebCamera : MonoBehaviour
 {
-    public InstantiateBoard MT;
-    private int counter = 0;
+  private Texture2D backgroundTexture;
+  private ARKit.Camera capture;
+  public ARKit.FeaturePoints fp = null;
+  public ARKit.InitialFrame ip = null;
+  public bool initialMatchDone = false;
+  public InstantiateBoard MT;
+  private int counter = 0;
 
-    private Texture2D backgroundTexture;
-    private ARKit.Camera capture;
-    static public ARKit.FeaturePoints fp = null;
-    static public ARKit.InitialFrame ip = null;
-    public bool initialMatchDone = false;
+  /*
+  private bool camAvailable;
+  private WebCamTexture webCam;
+  private Texture defaultBackground;
+  */
 
-    /*
-    private bool camAvailable;
-    private WebCamTexture webCam;
-    private Texture defaultBackground;
-    */
+  public RawImage background;
+  public AspectRatioFitter fit;
+  public Canvas canvas;
 
-    public RawImage background;
-    public AspectRatioFitter fit;
-    public Canvas canvas;
-
-    private void Start()
-    {
-        // defaultBackground = background.texture;
-        // WebCamDevice[] devices = WebCamTexture.devices;
-
+  private void Start()
+  {
+    // defaultBackground = background.texture;
+    // WebCamDevice[] devices = WebCamTexture.devices;
     /*
     if (devices.Length == 0)
     {
@@ -56,11 +54,19 @@ public class WebCamera : MonoBehaviour
     camAvailable = true;
     */
 
-    ARKit.Frame frame;
+    // ARKit.Frame frame;
 
-    this.capture = new ARKit.Camera(0, new ARKit.Size(1280, 720));
+    this.capture = new ARKit.Camera(1, new ARKit.Size(1080, 720));
 
-    frame = this.capture.GetNextFrame();
+    //setting the matched image
+    Emgu.CV.Mat frame = Emgu.CV.CvInvoke.Imread("match-external-webcam.jpg");
+
+    // ARKit.Frame newFrame = new ARKit.Frame(ARKit.Memory.Frame.Clone().Height, ARKit.Memory.Frame.Clone().Width, ARKit.Memory.Frame.Clone().)
+
+
+    //frame = ARKit.Memory.Frame.
+
+    //frame = this.capture.GetNextFrame();
 
     this.backgroundTexture = new Texture2D(frame.Width, frame.Height);
     background.texture = this.backgroundTexture; // set texture to webcam frames
@@ -69,17 +75,18 @@ public class WebCamera : MonoBehaviour
 
     if (System.IO.File.Exists("intrinsics.yml"))
     {
-      ip = new ARKit.InitialFrame();
-      ip.ReadFromFile();
+      this.ip = new ARKit.InitialFrame();
+      this.ip.ReadFromFile();
     }
     else
     {
-      ip = new ARKit.InitialFrame(this.capture, new ARKit.Size(4, 7), 30);
-      ip.Start();
+      this.ip = new ARKit.InitialFrame(this.capture, new ARKit.Size(4, 7), 30);
+      this.ip.Start();
     }
 
     ARKit.FeaturePoints.ComputeAndSave("simpsons-orig.jpg", "Assets/keypoints.yml");
-    fp = ARKit.FeaturePoints.ReadData("Assets/keypoints.yml", ip.Homography);
+    this.fp = ARKit.FeaturePoints.ReadData("Assets/keypoints.yml");
+    ARKit.Memory.Frame = frame;
   }
 
 
@@ -99,52 +106,63 @@ public class WebCamera : MonoBehaviour
         int orient = -webCam.videoRotationAngle;
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
         */
-        //GameObject g = GameObject.FindGameObjectWithTag("MainTrigger");
-        //MT = g.GetComponent<InstantiateBoard>();
+        GameObject g = GameObject.FindGameObjectWithTag("MainTrigger");
+        MT = g.GetComponent<InstantiateBoard>();
 
         bool tracking = false;
 
     if (ip != null && fp != null)
     {
-      if (!ip.Homography.IsEmpty)
+      //if (!ip.Homography.IsEmpty)
       {
-        var frame = this.capture.GetNextFrame();
+        //  var frame = this.capture.GetNextFrame();
 
-        if (!initialMatchDone)
-        {
-          fp.ComputeAndMatch();
-          initialMatchDone = true;
-        }
-        else
-          tracking = fp.TrackObject();
+        //if (!initialMatchDone)
+        //{
+        //  fp.ComputeAndMatch();
+        //  initialMatchDone = true;
+        //}
+        //else
+        //  tracking = fp.TrackObject();
 
-        if (fp.FindObject(!tracking))
-        {
-          if (fp.GetPose(ip.CameraMatrix, ip.DistortionCoefficients, out Emgu.CV.Mat rotations, out Emgu.CV.Mat translations))
-          {
-            frame = fp.DrawObjectBorder(true, ip.CameraMatrix, ip.DistortionCoefficients, rotations, translations);
-          }
-          else
-            frame = fp.DrawObjectBorder();
+        //if (fp.FindObject(!tracking))
+        //{
+        //  if (fp.GetPose(ip.CameraMatrix, ip.DistortionCoefficients, out Emgu.CV.Mat rotations, out Emgu.CV.Mat translations))
+        //  {
+        //    frame = fp.DrawObjectBorder(true, ip.CameraMatrix, ip.DistortionCoefficients, rotations, translations);
+        //  }
+        //  else
+        //    frame = fp.DrawObjectBorder();
 
-          print("object found");
-                    MT.objectFound = true;
-                    counter = 0;
-        }
-                else {
-                    print("object not found");
-                    if(counter >= 30) {
-                        MT.objectFound = false;
-                    }
-                    counter++;                    
-                }
+      this.fp.ComputeAndMatch();
 
-        this.backgroundTexture.LoadImage(frame.Image);
+        bool found = this.fp.FindObject();
+        MT.objectFound = found;
+        print("object found " + found);
+        ARKit.Frame f = this.fp.DrawObjectBorder();
+
+
+        //using (Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> nextFrame = ARKit.Memory.Frame.ToImage<Emgu.CV.Structure.Bgr, byte>())
+        //{
+        //  System.Drawing.Bitmap currentFrame = nextFrame.ToBitmap();
+        //  System.IO.MemoryStream m = new System.IO.MemoryStream();
+        //  System.Drawing.Imaging.ImageFormat format = currentFrame.RawFormat;
+
+        //  /*
+        //   * was throwing encoder cannot be null for formats that are MemoryBmp,
+        //   * found and fixed by referring to https://stackoverflow.com/questions/25242728/image-save-throws-exception-value-cannot-be-null-r-nparameter-name-encoder
+        //   */
+        //  currentFrame.Save(m, format);
+        //}
+
+
+
+        this.backgroundTexture.LoadImage(f.Image);
 
 
 
         Camera cam = Camera.main;
-        if (fp.GetHomography(out Emgu.CV.Mat H))
+        if (this.fp.GetHomography(out Emgu.CV.Mat H))
         {
           Emgu.CV.Matrix<double> H_mat = new Emgu.CV.Matrix<double>(3, 3);
           Emgu.CV.Matrix<double> cam_mat = new Emgu.CV.Matrix<double>(3, 3);
@@ -153,8 +171,7 @@ public class WebCamera : MonoBehaviour
             for (int j = 0; j < 3; j++)
             {
               double val = ARKit.MatExtension.GetValue(H, i, j);
-              print("i: " + i.ToString());
-              print("j: " + j.ToString());
+
               H_mat[i, j] = val;
 
               val = ARKit.MatExtension.GetValue(ip.CameraMatrix, i, j);
@@ -166,6 +183,7 @@ public class WebCamera : MonoBehaviour
 
           Matrix4x4 proj_mat = Matrix4x4.identity;
 
+          print("rows " + proj.Rows + " cols " + proj.Cols);
 
           for (int i = 0; i < 4; i++)
           {
@@ -174,16 +192,17 @@ public class WebCamera : MonoBehaviour
               double val = proj[i, j];
               int val_proj = (int)val;
 
+              print("i: " + i.ToString() + " j: " + j.ToString());
+
               proj_mat[i, j] = val_proj;
             }
           }
 
           cam.projectionMatrix = proj_mat;
           print("projection matrix set");
+          print("euler angles " + cam.projectionMatrix.rotation.ToString());
 
         }
-
-
       }
     }
   }
